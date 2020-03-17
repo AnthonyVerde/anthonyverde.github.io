60d93f6a4582544c84b34064ee42f5891b7d4cac
Verde <a.green.designer@gmail.com>
2020-03-17 08:02:50 -0400

    added new projects

--- assets/javascript/video-modal.js
+++ assets/javascript/video-modal.js
@@ -1,19 +1,26 @@
 $(document).ready(function () {
-    //$('#myModal').on('shown.bs.modal', function () {
-    //    $('#video1')[0].play();
-    //})
+
     $('#myModal').on('hidden.bs.modal', function () {
         $('#video1')[0].pause();
     })
 
-    //$('#myModal2').on('shown.bs.modal', function () {
-    //    $('#video2')[0].play();
-    //})
     $('#myModal2').on('hidden.bs.modal', function () {
         $('#video2')[0].pause();
     })
 
     $('#myModal3').on('hidden.bs.modal', function () {
         $('#video3')[0].pause();
+    })
+
+    $('#myModal4').on('hidden.bs.modal', function () {
+        $('#video4')[0].pause();
+    })
+
+    $('#myModal5').on('hidden.bs.modal', function () {
+        $('#video5')[0].pause();
+    })
+
+    $('#myModal6').on('hidden.bs.modal', function () {
+        $('#video6')[0].pause();
     })
 });
